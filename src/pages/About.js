814b8c6f179293e1dd8ad765dcd7fd207a33c727
Stephanie Bolder <stephanie@Stephanies-MacBook-Pro.local>
2021-12-15 19:22:18 +0100

    added logo

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,10 @@
 import Title from "../components/Title";
-
+import logo from '../assets/logo192.png'
 export default function About() {
     return (
         <div>
             <Title title="About"/>
+            <img src={logo} />
         </div>
     )
 }
